b20a5be605067a65f63a306fcc5b313ad7e51d15
Orion <orion45.dd@gmail.com>
2024-08-15 10:40:14 -0700

    Basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A type-annotated function add"""
+
+
+def add(a: float, b: float) -> float:
+    """returns sum as a float"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returna take in two string arguments and concatenate the strings"""
+    return str1 + str2
