8ba7af410e0bb1d859365a3102db1cb50477bc34
Orion <Orion45.dd@gmail.com>
2024-08-20 21:57:47 +0000

    return type of generator by the generic type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,8 +3,11 @@
 import asyncio
 import random
 
+from typing import Generator
+'''Return type of generator by the generic type yield_type'''
 
-async def async_generator():
+
+async def async_generator() -> Generator[int, None, None]:
     """loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10"""
     for a in range(10):
